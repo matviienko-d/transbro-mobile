33937be6838c01e26e6c4166bbbdb1d8176b5d0c
Danylo Matviienko <danylom@valcre.com>
2023-06-25 16:46:37 +0300

    [TB-0] base language picker

---
+++ components/LanguagePicker.jsx
@@ -0,0 +1,74 @@
+import {useState} from "react";
+import DropDownPicker from "react-native-dropdown-picker";
+import styled from "styled-components/native";
+import {useFonts} from "expo-font";
+
+const LanguageBar = styled.View`
+  justifyContent: space-between;
+  flexDirection: row;
+  margin-top: 16px;
+  width: 100%;
+`;
+
+const DropdownItem = styled.View`
+  width: 43%;
+`;
+
+export const LanguagePicker = ({languageItems}) => {
+    const [inputLanguageOpen, setInputLanguageOpen] = useState(false);
+    const [outputLanguageOpen, setOutputLanguageOpen] = useState(false);
+    const [inputLanguageValue, setInputLanguageValue] = useState('uk');
+    const [outputLanguageValue, setOutputLanguageValue] = useState('en');
+    const [items, setItems] = useState([...languageItems]);
+
+    const onInputLanguageOpen = () => {
+        setOutputLanguageOpen(false);
+    };
+
+    const onOutputLanguageOpen = () => {
+        setInputLanguageOpen(false);
+    };
+
+    return (
+        <LanguageBar>
+            <DropdownItem>
+                <DropDownPicker
+                    style={{
+                        backgroundColor: '#FAFAFA',
+                        border: '1px solid rgba(158, 158, 158, .25)'
+                    }}
+                    open={inputLanguageOpen}
+                    value={inputLanguageValue}
+                    items={items}
+                    setOpen={setInputLanguageOpen}
+                    setValue={setInputLanguageValue}
+                    setItems={setItems}
+                    onOpen={onInputLanguageOpen}
+                    schema={{
+                        label: 'name',
+                        value: 'code'
+                    }}
+                />
+            </DropdownItem>
+            <DropdownItem>
+                <DropDownPicker
+                    style={{
+                        backgroundColor: '#FAFAFA',
+                        border: '1px solid rgba(158, 158, 158, .25)'
+                    }}
+                    open={outputLanguageOpen}
+                    value={outputLanguageValue}
+                    items={items}
+                    setOpen={setOutputLanguageOpen}
+                    setValue={setOutputLanguageValue}
+                    setItems={setItems}
+                    onOpen={onOutputLanguageOpen}
+                    schema={{
+                        label: 'name',
+                        value: 'code'
+                    }}
+                />
+            </DropdownItem>
+        </LanguageBar>
+    );
+}
