958c8fdc5d4e830494d3facbd1d1e75ef0bc2276
Danylo Matviienko <danylom@valcre.com>
2024-06-23 16:52:15 +0300

    [TB-2] mocked translation api

---
+++ mocks/languages.js
@@ -0,0 +1,66 @@
+export const LANGUAGES_ITEMS_LIST = {
+    data: {
+        "translation": {
+            "fr": {
+                "name": "French",
+                "nativeName": "Français",
+                "dir": "ltr"
+            },
+            "en": {
+                "name": "English",
+                "nativeName": "English",
+                "dir": "ltr"
+            },
+            "es": {
+                "name": "Spanish",
+                "nativeName": "Español",
+                "dir": "ltr"
+            },
+            "zh": {
+                "name": "Chinese",
+                "nativeName": "中文",
+                "dir": "ltr"
+            },
+            "hi": {
+                "name": "Hindi",
+                "nativeName": "हिन्दी",
+                "dir": "ltr"
+            },
+            "ar": {
+                "name": "Arabic",
+                "nativeName": "العربية",
+                "dir": "rtl"
+            },
+            "pt": {
+                "name": "Portuguese",
+                "nativeName": "Português",
+                "dir": "ltr"
+            },
+            "ru": {
+                "name": "Russian",
+                "nativeName": "Русский",
+                "dir": "ltr"
+            },
+            "ja": {
+                "name": "Japanese",
+                "nativeName": "日本語",
+                "dir": "ltr"
+            },
+            "de": {
+                "name": "German",
+                "nativeName": "Deutsch",
+                "dir": "ltr"
+            },
+            "ko": {
+                "name": "Korean",
+                "nativeName": "한국어",
+                "dir": "ltr"
+            },
+            "uk": {
+                "name": "Ukrainian",
+                "nativeName": "Українська",
+                "dir": "ltr"
+            }
+        }
+    }
+}
--- screens/Home.jsx
+++ screens/Home.jsx
@@ -8,65 +8,66 @@ import {LanguagePicker} from "../components/LanguagePicker";
 import debounce from "lodash.debounce";
 import {convertLanguageItemsToList} from "../utils/language-items";
 import {activeLanguagesContext} from "../providers/ActiveLanguagesProvider";
+import {LANGUAGES_ITEMS_LIST} from "../mocks/languages";
 
 const Logo = styled.Text`
-  fontSize: 25px;
-  fontFamily: "Raleway-Bold";
-  color: #9E9E9E;
-  fontWeight: 700;
-  text-align: center;
+    fontSize: 25px;
+    fontFamily: "Raleway-Bold";
+    color: #9E9E9E;
+    fontWeight: 700;
+    text-align: center;
 `;
 
 const Header = styled.View`
-  padding: 20px 12px;
-  width: 100%;
-  background-color: #fafafa;
-  border-bottom-width: 1px;
-  border-bottom-style: solid;
-  border-bottom-color: rgba(158, 158, 158, .5);
-  z-index: 100;
+    padding: 20px 12px;
+    width: 100%;
+    background-color: #fafafa;
+    border-bottom-width: 1px;
+    border-bottom-style: solid;
+    border-bottom-color: rgba(158, 158, 158, .5);
+    z-index: 100;
 `;
 
 const MainView = styled.View`
-  flex: 1;
-  backgroundColor: #fff;
-  alignItems: center;
-  justifyContent: flex-start;
+    flex: 1;
+    backgroundColor: #fff;
+    alignItems: center;
+    justifyContent: flex-start;
 `;
 
 const MainContent = styled.View`
-  width: 100%;
-  overflow-y: scroll;
-  z-index: 0;
+    width: 100%;
+    overflow-y: scroll;
+    z-index: 0;
 `;
 
 const SpinnerView = styled.View`
-  padding: 20px 0 12px 0;
+    padding: 20px 0 12px 0;
 `;
 
 const InputToTranslate = styled.TextInput`
-  width: 100%;
-  min-height: 350px;
-  padding: 10px 12px;
-  text-align: left;
-  fontFamily: "Bitter-Regular";
-  font-size: 18px;
-  font-weight: 500;
-  border-bottom-width: 1px;
-  borderBottomStyle: 'solid';
-  border-bottom-color: rgba(158, 158, 158, .5);
+    width: 100%;
+    min-height: 350px;
+    padding: 10px 12px;
+    text-align: left;
+    fontFamily: "Bitter-Regular";
+    font-size: 18px;
+    font-weight: 500;
+    border-bottom-width: 1px;
+    borderBottomStyle: 'solid';
+    border-bottom-color: rgba(158, 158, 158, .5);
 `;
 
 const TranslationResultText = styled.Text`
-  fontSize: 18px;
-  fontFamily: "Raleway-Bold";
-  color: #000;
-  fontWeight: 700;
-  padding: 20px 0 12px 0;
-  margin: 0 12px;
-  border-bottom-width: ${props => props.isEmpty ? '1px' : '0'};
-  border-bottom-style: solid;
-  border-bottom-color: rgba(158, 158, 158, .5);
+    fontSize: 18px;
+    fontFamily: "Raleway-Bold";
+    color: #000;
+    fontWeight: 700;
+    padding: 20px 0 12px 0;
+    margin: 0 12px;
+    border-bottom-width: ${props => props.isEmpty ? '1px' : '0'};
+    border-bottom-style: solid;
+    border-bottom-color: rgba(158, 158, 158, .5);
 `;
 
 export const Home = () => {
@@ -77,7 +78,8 @@ export const Home = () => {
     const [inputLanguageValue, setInputLanguageValue, outputLanguageValue, setOutputLanguageValue] = useContext(activeLanguagesContext);
 
     const fetchLanguageItems = () => {
-        axios.get(`${MICROSOFT_TRANSLATOR_URL}/languages`, {
+        // TODO: mocked until microsoft subscription update
+        /*axios.get(`${MICROSOFT_TRANSLATOR_URL}/languages`, {
             params: {
                 'api-version': API_VERSION
             },
@@ -85,6 +87,9 @@ export const Home = () => {
                 'Ocp-Apim-Subscription-Key': 'b395cb02cea349fe94a98f2e43d4ffb4',
                 'Ocp-Apim-Subscription-Region': 'westeurope',
             },
+        })*/
+        new Promise(resolve => {
+            setTimeout(() => resolve(LANGUAGES_ITEMS_LIST), 1000)
         })
             .then(({data: {translation}}) => {
                 setLanguageItems(convertLanguageItemsToList(translation));
@@ -96,7 +101,8 @@ export const Home = () => {
 
     const translateText = (textToTranslate) => {
         setLoading(true);
-        axios.post(`${MICROSOFT_TRANSLATOR_URL}/translate`,
+        // TODO: mocked until microsoft subscription update
+        /*axios.post(`${MICROSOFT_TRANSLATOR_URL}/translate`,
             [
                 {
                     text: textToTranslate,
@@ -112,10 +118,18 @@ export const Home = () => {
                     'Ocp-Apim-Subscription-Region': 'westeurope',
                     'Content-Type': 'application/json',
                 },
-            })
-            .then(({data}) => {
+            })*/
+        new Promise(resolve => {
+            setTimeout(() => resolve(textToTranslate
+                ? `Translated from: ${textToTranslate}`
+                : ''
+            ), 1000)
+        })
+            .then((translatedText) => {
                 setLoading(false);
-                setTranslatedText(data[0].translations[0].text);
+                setTranslatedText(translatedText);
+                // TODO: mocked until microsoft subscription update
+                // setTranslatedText(data[0].translations[0].text);
             })
             .catch((error) => {
                 console.error(error);
@@ -138,50 +152,51 @@ export const Home = () => {
         translateText(textToTranslate);
     }, [inputLanguageValue, outputLanguageValue]);
 
-/*    if (!fontsLoaded) {
-        console.error('Fonts are not loaded!');
-    }*/
+    /*    if (!fontsLoaded) {
+            console.error('Fonts are not loaded!');
+        }*/
 
     return (
         <SafeAreaView style={{flex: 1}}>
-            <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
-                <MainView>
-                    <Header>
-                        <Logo>transbro</Logo>
-                        <View>
-                            {
-                                languageItems?.length ?
-                                    <LanguagePicker
-                                        languageItems={languageItems}
-                                    >
-                                    </LanguagePicker> : null
-                            }
-                        </View>
-                    </Header>
-                    <MainContent>
-                        <InputToTranslate
-                            multiline={true}
-                            value={textToTranslate}
-                            onChangeText={handleTranslateInputChange}
-                            placeholder={'Translate...'}
-                        />
-                        {isLoading ?
-                            <SpinnerView>
-                                <ActivityIndicator
-                                    color='#9E9E9E'
-                                    size="large"
-                                />
-                            </SpinnerView> : null
+            <MainView>
+                <Header>
+                    <Logo>transbro</Logo>
+                    <View>
+                        {
+                            languageItems?.length ?
+                                <LanguagePicker
+                                    languageItems={languageItems}
+                                >
+                                </LanguagePicker> : null
                         }
-                        {!isLoading ?
-                            <TranslationResultText
-                                isEmpty={!!translatedText}
-                            >{translatedText}</TranslationResultText> : null
-                        }
-                    </MainContent>
-                    <StatusBar style="auto"/>
-                </MainView>
-            </TouchableWithoutFeedback>
+                    </View>
+                </Header>
+                <MainContent>
+                    <InputToTranslate
+                        multiline={true}
+                        value={textToTranslate}
+                        onChangeText={handleTranslateInputChange}
+                        placeholder={'Translate...'}
+                    />
+                    {isLoading ?
+                        <SpinnerView>
+                            <ActivityIndicator
+                                color='#9E9E9E'
+                                size="large"
+                            />
+                        </SpinnerView>
+                        : <TranslationResultText
+                            isEmpty={!!translatedText}
+                        >{translatedText}</TranslationResultText>
+                    }
+                    {/*{!isLoading ?
+                        <TranslationResultText
+                            isEmpty={!!translatedText}
+                        >{translatedText}</TranslationResultText> : null
+                    }*/}
+                </MainContent>
+                <StatusBar style="auto"/>
+            </MainView>
         </SafeAreaView>
     );
 }
